805b7d72ef085c4994ffcdf9a8baf00c61feb971
Natasha9191 <133143274+Natasha9191@users.noreply.github.com>
2024-02-10 21:42:19 +0530

    first commit

---
+++ www.aiseesoft.com/online-bg-remover/editor/static/js/url-setting.js
@@ -0,0 +1,56 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: zm
+ * @Date: 2023-08-29 17:23:33
+ * @LastEditors: zm
+ * @LastEditTime: 2023-08-30 14:35:20
+ */
+var hostUrl = 'www.aiseesoft.com';
+var hostUrlFr = 'www.aiseesoft.fr';
+var hostUrlDe = 'www.aiseesoft.de';
+var hostUrlJp = 'www.aiseesoft.jp';
+var hostUrlZhcn = 'zh-cn.aiseesoft.com';
+var hostUrlZhtw = 'zh-tw.aiseesoft.com';
+var hostUrlCs = 'cs.aiseesoft.com';
+var hostUrlDa = 'da.aiseesoft.com';
+var hostUrlNl = 'nl.aiseesoft.com';
+var hostUrlFi = 'fi.aiseesoft.com';
+var hostUrlEl = 'el.aiseesoft.com';
+var hostUrlIt = 'it.aiseesoft.com';
+var hostUrlNo = 'no.aiseesoft.com';
+var hostUrlPl = 'pl.aiseesoft.com';
+var hostUrlPt = 'pt.aiseesoft.com';
+var hostUrlRu = 'ru.aiseesoft.com';
+var hostUrlEs = 'es.aiseesoft.com';
+var hostUrlSv = 'sv.aiseesoft.com';
+var hostUrlHu = 'hu.aiseesoft.com';
+var hostUrlTr = 'tr.aiseesoft.com';
+
+var accountHref="https://account.aiseesoft.com/";
+
+var accountHrefDev="https://sandboxaccount.aiseesoft.com/";
+
+var isDe_Fr_Ja=true;
+var pricing={
+    "de":{
+        "symbol":'€',//价格单位
+        "place":true,//false 展示在价格前面 true展示在价格后面
+        "vid":116505
+    },
+    "fr":{
+        "symbol":'€',
+        "place":true,
+        "vid":116504
+    },
+    "ja":{
+        "symbol":'￥',
+        "place":false,
+        "vid":116506
+    },
+    "en":{
+        "symbol":'$',
+        "place":false,
+        "vid":115664
+    }
+}
